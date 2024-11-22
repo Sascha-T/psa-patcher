05d558999ff0757bb08d2bae22f767bf0ae5a9da
Sascha T <info@sascha-t.de>
2024-11-22 22:06:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const { globSync } = require("glob");
+const fs = require("fs")
+
+let processed = []
+
+function processFile(fileName) {
+    if(fs.existsSync(fileName + ".bak")) {
+        processed.push(fileName);
+        console.log(`${fileName} already processed.`)
+        return;
+    }
+    let data = fs.readFileSync(fileName, "utf8")
+    if(!data.includes("TestInternet")) {
+        console.log(`${fileName} requires no processing.`)
+        return;
+    }
+
+    fs.copyFileSync(fileName, fileName + ".bak")
+
+    data = data.replace(/chk="\d*"\x20/g, "");
+
+    let lines = data.split("\r\n");
+    
+    let tests = []
+    let acts = []
+
+    for(let i = 0; i < lines.length; i++) {
+
+        // detect all test internets
+        let line = lines[i];
+        if(line.startsWith("<TestInternet")) {
+            for(let x = i; x >= 0; x--) {
+
+                let line2 = lines[x]; // now find the act
+                if(line2.startsWith("<Act ")) {
+                    let destc = parseInt([...line2.matchAll(/destc="?(\d*)"?/g)][0][1]);
+                    let destl = parseInt([...line2.matchAll(/destl="?(\d*)"?/g)][0][1]);
+                    acts.push({
+                        destc, destl
+                    })
+                    break;
+                }
+            }
+        }
+        if(line.startsWith("<Test ")) {
+            let posc = parseInt([...line.matchAll(/posc="?(\d*)"?/g)][0][1]);
+            let posl = parseInt([...line.matchAll(/posl="?(\d*)"?/g)][0][1]);
+            tests.push({
+                line: i, posc, posl
+            })
+        }
+   }
+
+   let patches = 0
+
+   for(let act of acts) {
+        for(let test of tests) {
+            if(test.posc == act.destc && test.posl == act.destl) {
+                // process
+                let testLine = lines[test.line]; 
+
+                let destyesc = parseInt([...testLine.matchAll(/destyesc="?(\d*)"?/g)][0][1]);
+                let destyesl = parseInt([...testLine.matchAll(/destyesl="?(\d*)"?/g)][0][1]);
+
+                // execute order 66
+                testLine = testLine.replace(/destnoc=(")?(\d*)(")?/g, `destnoc=$1${destyesc}$3`)
+                testLine = testLine.replace(/destnol=(")?(\d*)(")?/g, `destnol=$1${destyesl}$3`)
+
+                lines[test.line] = testLine;
+                patches++;
+            }
+        }
+   }
+
+   let output = "";
+   for(let line of lines) output += line + "\r\n";
+
+   fs.writeFileSync(fileName + ".out", output, "utf8")
+   console.log(`Patched ${patches} internet check(s) in ${fileName}`)
+
+   processed.push(fileName);
+}
+
+const playbooks = globSync("**/*.s")
+for (const element of playbooks) {
+    processFile(element)
+}
+fs.writeFileSync("_psaPatcher.json", JSON.stringify(processed, null, 4), "utf-8")
---
+++ readme.md
@@ -0,0 +1,14 @@
+# psa hates me
+i love them tho :3
+
+## internet checks?
+for nerds, you know what you're doing, you shouldn't blow up your car (or do other illegal stuff, just don't have skill issue with diesels?)
+
+# i'm not responsible for literally anything because i'm quite irresponsible
+
+# PSA Patcher
+This thing will remove the internet checks out of all "playbooks" provided with a DiagBox compatible collection of "playbooks". \
+This would theoretically allow you to access menus in DiagBox normally locked behind an internet connection, but will not make anything possible that requires data from PSA servers (duh). \
+Please use responsibly in consideration of your own personal safety and that of others that may be so unfortunate to end up behind/in front of you on the highway or any other street.
+
+It will also produce a file called `_psaPatcher.json` which you could use to revert all patches (didn't write a program for that yet) so you don't have to reinstall DiagBox if this breaks your install.
